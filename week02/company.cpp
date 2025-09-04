5873214899b2b640dd2f4179798be61275c4422d
이원서 <wonseo>
2025-09-04 11:26:12 +0900

    v0.3

---
+++ week02/company.cpp
@@ -0,0 +1,11 @@
+﻿#include "company.h"
+
+Company::Company(string name, string telephone) : name(name), telephone(telephone)
+{
+}
+
+void Company::print() const
+{
+	cout << "ȸ��� : " << name << endl;
+	cout << "����ó : " << telephone << endl;
+}
