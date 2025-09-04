4df94df867bb300b4ca0e48b7221f4820d76afec
이원서 <wonseo>
2025-09-04 11:05:24 +0900

    README.md & .gitignore

---
+++ week02/app02.cpp
@@ -0,0 +1,18 @@
+#include "invoice.h"
+
+
+int main()
+{
+
+	Product product1("Table", 150.00);
+	Product product2("Chair", 80.00);
+
+	Invoice invoice(1001);  // aggregation X
+
+	invoice.add(1, product1);
+	invoice.add(4, product2);
+
+	invoice.print();
+
+	return 0;
+}
