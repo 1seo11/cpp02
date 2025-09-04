5bb6b717f32f882e981e4d7749becdc91f2cad43
이원서 <wonseo>
2025-09-04 11:17:25 +0900

    v0.2

---
+++ week02/invoice.cpp
@@ -0,0 +1,18 @@
+﻿#include "invoice.h"
+
+Invoice::Invoice(int invNum)
+	: invoiceNumber(invNum), invoiceTotal(0.0)
+{
+}
+Invoice::~Invoice()
+{
+}
+void Invoice::add(int quantity, Product product)
+{
+	invoiceTotal += quantity * product.getPrice();
+}
+void Invoice::print() const
+{
+	cout << "û�� ��ȣ: " << invoiceNumber << endl;
+	cout << "û�� �ݾ�: " << invoiceTotal << endl;
+}
